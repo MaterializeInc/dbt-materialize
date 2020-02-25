d9d6d1e883d80f36bfe45688802f04745b16078a
Josh Wills <jwills@apache.org>
2020-02-25 10:13:09 -0800

    Setup and README

---
+++ README.md
@@ -0,0 +1,12 @@
+## dbt-materialize
+
+### Installation
+This plugin can be installed via pip:
+```
+$ pip install dbt-materialize
+```
+
+### Configuring your profile
+
+[Materialize](http://materialize.io) is based on the Postgres database protocols, so use the [dbt postgres settings](https://docs.getdbt.com/docs/profile-postgres)
+in your connection profile, only substitute `type: materialize` for `type: postgres`.
--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 from setuptools import find_packages
 from distutils.core import setup
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 package_name = "dbt-materialize"
-package_version = "0.0.1"
+package_version = "0.14.3"
 description = """The materialize adpter plugin for dbt (data build tool)"""
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
-    long_description=description,
-    author=<INSERT AUTHOR HERE>,
-    author_email=<INSERT EMAIL HERE>,
-    url=<INSERT URL HERE>,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author='Josh Wills',
+    author_email='joshwills+dbt@gmail.com',
+    url='https://github.com/jwills/dbt-materialize',
     packages=find_packages(),
     package_data={
         'dbt': [
@@ -22,7 +29,6 @@ setup(
         ]
     },
     install_requires=[
-        dbt-core==0.13.0,
-        <INSERT DEPENDENCIES HERE>
+        'dbt-core=={}'.format(package_version),
     ]
 )
