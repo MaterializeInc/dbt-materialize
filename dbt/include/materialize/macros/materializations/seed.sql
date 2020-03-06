8d492385a17526c1814c4f29605e328ec46590ca
Josh Wills <jwills@apache.org>
2020-03-05 23:11:32 -0800

    Get a working version of loading seed data into materialize.io

--- dbt/include/materialize/macros/materializations/seed.sql
+++ dbt/include/materialize/macros/materializations/seed.sql
@@ -1,18 +1,23 @@
-{% macro materialize__load_csv_rows(model) %}
-
-  {% set agate_table = model['agate_table'] %}
-  {% set bindings = agate_table.rows %}
+{% macro materialize__load_csv_rows(model, agate_table) -%}
   {% set cols_sql %}
-    {%- for i, column in enumerate(agate_table.column_names) -%}
-      column{{i}} as {{column}}
+    {% for column in agate_table.column_names %}
+      column{{loop.index}} as {{column}}
       {%- if not loop.last%},{%- endif %}
     {% endfor %}
   {% endset %}
 
+  {% set bindings = [] %}
+  {% for chunk in agate_table.rows | batch(1000) %}
+        {% for row in chunk %}
+            {% do bindings.extend(row) %}
+        {% endfor %}
+  {% endfor %}
+
   {% set sql %}
-    create materialized view {{ this.render(False) }} AS (
+    -- {{ bindings | length }}
+    create materialized view {{ this.render() }} AS (
       select {{ cols_sql }} from (VALUES
-      {% for row in bindings -%} 
+      {% for chunk in agate_table.rows | batch(1)  -%} 
         ({%- for column in agate_table.column_names -%}
             %s
             {%- if not loop.last%},{%- endif %}
@@ -20,26 +25,28 @@
         {%- if not loop.last%},{%- endif %}
       {%- endfor %}         
       ) AS tbl
-    );
+    )
   {% endset %}
 
-  {% set _ = adapter.add_query(sql, bindings=bindings, abridge_sql_log=True) %}
+  {% do adapter.add_query(sql, bindings=bindings, abridge_sql_log=True) %}
   {{ return(sql) }}
-{% endmacro %}
 
-{% macro materialize__reset_csv_table(model, full_refresh, old_relation) %}
+{%- endmacro %}
+
+{% macro materialize__reset_csv_table(model, full_refresh, old_relation, agate_table) -%}
     {% if old_relation %}
         {{ adapter.drop_relation(old_relation) }}
     {% endif %}
-    {% set sql = create_csv_table(model) %}
+    {% set sql = create_csv_table(model, agate_table) %}
     {{ return(sql) }}
-{% endmacro %}
+{%- endmacro %}
 
 {% materialization seed, adapter='materialize' %}
 
   {%- set identifier = model['alias'] -%}
   {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
-  {%- set csv_table = model["agate_table"] -%}
+  {%- set agate_table = load_agate_table() -%}
+  {%- do store_result('agate_table', status='OK', agate_table=agate_table) -%}
 
   {{ run_hooks(pre_hooks, inside_transaction=False) }}
 
@@ -48,10 +55,9 @@
 
   -- build model
   {% set status = 'CREATE' %}
-  {% set num_rows = (csv_table.rows | length) %}
-  {% set sql = load_csv_rows(model) %}
+  {% set sql = load_csv_rows(model, agate_table) %}
 
-  {% call noop_statement('main', status ~ ' ' ~ num_rows) %}
+  {% call noop_statement('main', status) %}
     -- dbt seed --
     {{ sql }}
   {% endcall %}
@@ -60,4 +66,7 @@
   -- `COMMIT` happens here
   {{ adapter.commit() }}
   {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+  {% set target_relation = this.incorporate(type='table') %}
+  {{ return({'relations': [target_relation]}) }}
 {% endmaterialization %}
